05b07f981d53a823710c31bd11f4e9fd35e37957
Artur Mantsevich <arturmantsevich@gmail.com>
2025-10-03 21:25:20 +0200

    setup basename for deployed app

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,11 @@ import "./index.css";
 import App from "./App.tsx";
 import { store } from "./store";
 
+const basename = import.meta.env.PROD ? "/of-frontend-task" : "/";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <Provider store={store}>
         <App />
       </Provider>
